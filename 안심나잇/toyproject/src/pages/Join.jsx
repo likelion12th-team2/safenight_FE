5ab2f158b44bca6cf62237667e2b6efbd500f3b2
hahee <hahee_hi@naver.com>
2024-07-11 22:50:12 +0900

    확인해주세요!

--- 안심나잇/toyproject/src/pages/Join.jsx
+++ 안심나잇/toyproject/src/pages/Join.jsx
@@ -2,9 +2,35 @@ import React from "react";
 import { useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import * as J from "../styles/styledJoin";
+import axios from "axios";
 
 const Join = ({ hour, min }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirm, setPasswordConfirm] = useState("");
+  const [message, setMessage] = useState("");
+  const [username, setUsername] = useState("");
+  const [memberType, setMemberType] = useState(""); // 회원 선택 (환자, 간병인, 선택하지 않음)
+  const [agreeTerms, setAgreeTerms] = useState(false); // 약관 동의
+
   const navigate = useNavigate();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await axios.post("http://127.0.0.1:8000/user/", {
+        email: email,
+        password: password,
+        username: username,
+        member_type: memberType,
+      });
+      setMessage("회원가입 성공!");
+      navigate("/login/success");
+    } catch (error) {
+      setMessage("회원가입 실패: " + error.message);
+    }
+  };
+
   const handleDivClick = (path) => {
     navigate(path);
   };
@@ -39,131 +65,151 @@ const Join = ({ hour, min }) => {
           <div id="join_title2">회원가입</div>
         </div>
       </J.Title>
-      <J.Join_email>
-        <div id="email">이메일</div>
-        <input
-          type="text"
-          id="email_text"
-          placeholder="이메일을 입력해 주세요"
-        />
-        <img
-          id="check_img"
-          src={`${process.env.PUBLIC_URL}/photos/join_purple.svg`}
-        />
-        <div id="line1"></div>
-        <div id="pw">비밀번호</div>
-        <input
-          type="text"
-          id="email_text"
-          placeholder="8 ~ 20자 영문 대,소문자,숫자,특수문자 조합"
-        />
-        <img
-          id="check_img"
-          src={`${process.env.PUBLIC_URL}/photos/join_gray.svg`}
-        />
-        <div id="line1"></div>
-        <div id="pw">비밀번호 확인</div>
-        <input
-          type="text"
-          id="email_text"
-          placeholder="8 ~ 20자 영문 대,소문자,숫자,특수문자 조합"
-        />
-        <img
-          id="check_img"
-          src={`${process.env.PUBLIC_URL}/photos/join_gray.svg`}
-        />
-        <div id="line1"></div>
-        <div id="name_input">이름</div>
-        <input type="text" id="email_text" placeholder="이름을 입력해주세요" />
-        <div id="line1"></div>
-      </J.Join_email>
-      <J.checkBoxs>
-        <div id="member_select">
-          <div id="member_select_title">회원선택</div>
-          <div id="check">
-            <img
-              id="member_check"
-              src={`${process.env.PUBLIC_URL}/photos/gray.svg`}
-            />
-            <div id="member_text">환자</div>
+      <form onSubmit={handleSubmit}>
+        <J.Join_email>
+          <div id="email">이메일</div>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+            id="email_text"
+            placeholder="이메일을 입력해 주세요"
+          />
+          <img
+            id="check_img"
+            src={`${process.env.PUBLIC_URL}/photos/join_purple.svg`}
+          />
+          <div id="line1"></div>
+          <div id="pw">비밀번호</div>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+            id="email_text"
+            placeholder="8 ~ 20자 영문 대,소문자,숫자,특수문자 조합"
+          />
+          <img
+            id="check_img"
+            src={`${process.env.PUBLIC_URL}/photos/join_gray.svg`}
+          />
+          <div id="line1"></div>
+          <div id="pw">비밀번호 확인</div>
+          <input
+            type="password"
+            id="email_text"
+            placeholder="8 ~ 20자 영문 대,소문자,숫자,특수문자 조합"
+          />
+          <img
+            id="check_img"
+            src={`${process.env.PUBLIC_URL}/photos/join_gray.svg`}
+          />
+          <div id="line1"></div>
+          <div id="name_input">닉네임</div>
+          <input
+            type="text"
+            id="email_text"
+            placeholder="닉네임을 입력해주세요"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+          <div id="line1"></div>
+        </J.Join_email>
+        <J.checkBoxs>
+          <div id="member_select">
+            <div id="member_select_title">회원선택</div>
+            <div id="check">
+              <img
+                id="member_check"
+                src={`${process.env.PUBLIC_URL}/photos/gray.svg`}
+              />
+              <div id="member_text">환자</div>
+            </div>
+            <div id="check">
+              <img
+                id="member_check"
+                src={`${process.env.PUBLIC_URL}/photos/gray.svg`}
+              />
+              <div id="member_text">간병인</div>
+            </div>
+            <div id="check">
+              <img
+                id="member_check"
+                src={`${process.env.PUBLIC_URL}/photos/gray.svg`}
+              />
+              <div id="member_text">선택하지 않음</div>
+            </div>
           </div>
-          <div id="check">
-            <img
-              id="member_check"
-              src={`${process.env.PUBLIC_URL}/photos/gray.svg`}
-            />
-            <div id="member_text">간병인</div>
-          </div>
-          <div id="check">
-            <img
-              id="member_check"
-              src={`${process.env.PUBLIC_URL}/photos/gray.svg`}
-            />
-            <div id="member_text">선택하지 않음</div>
-          </div>
-        </div>
 
-        <div id="alarm">
-          <div id="alarm_check">알람 받기(선택)</div>
-          <div id="check">
-            <img
-              id="member_check"
-              src={`${process.env.PUBLIC_URL}/photos/gray.svg`}
-            />
-            <div id="member_text">동의</div>
+          <div id="alarm">
+            <div id="alarm_check">알람 받기(선택)</div>
+            <div id="check">
+              <img
+                id="member_check"
+                src={`${process.env.PUBLIC_URL}/photos/gray.svg`}
+              />
+              <div id="member_text">동의</div>
+            </div>
+            <div id="check">
+              <img
+                id="member_check"
+                src={`${process.env.PUBLIC_URL}/photos/gray.svg`}
+              />
+              <div id="member_text">미동의</div>
+            </div>
+            <div id="caution">동의하시면 카드뉴스 알림이 전송됩니다.</div>
           </div>
-          <div id="check">
-            <img
-              id="member_check"
-              src={`${process.env.PUBLIC_URL}/photos/gray.svg`}
-            />
-            <div id="member_text">미동의</div>
-          </div>
-          <div id="caution">동의하시면 카드뉴스 알림이 전송됩니다.</div>
-        </div>
 
-        <div id="terms_agree">
-          <div id="terms_agree_title">약관동의</div>
-          <div id="check">
-            <img
-              id="terms_check"
-              src={`${process.env.PUBLIC_URL}/photos/gray.svg`}
-            />
-            <div id="member_text">전체 동의합니다</div>
+          <div id="terms_agree">
+            <div id="terms_agree_title">약관동의</div>
+            <div id="check">
+              <img
+                id="terms_check"
+                src={`${process.env.PUBLIC_URL}/photos/gray.svg`}
+              />
+              <div id="member_text">전체 동의합니다</div>
+            </div>
+            <div id="line2"></div>
+            <div id="check1">
+              <img
+                id="terms_check"
+                src={`${process.env.PUBLIC_URL}/photos/gray.svg`}
+              />
+              <div id="terms_text">이용약관에 동의합니다 (필수)</div>
+            </div>
+            <div id="check1">
+              <img
+                id="terms_check"
+                src={`${process.env.PUBLIC_URL}/photos/gray.svg`}
+              />
+              <div id="terms_text">
+                개인정보 수집 및 이용에 동의합니다 (필수)
+              </div>
+            </div>
+            <div id="check1">
+              <img
+                id="terms_check"
+                src={`${process.env.PUBLIC_URL}/photos/gray.svg`}
+              />
+              <div id="member_text">위치정보 수집에 동의합니다 (선택)</div>
+            </div>
           </div>
-          <div id="line2"></div>
-          <div id="check1">
-            <img
-              id="terms_check"
-              src={`${process.env.PUBLIC_URL}/photos/gray.svg`}
-            />
-            <div id="terms_text">이용약관에 동의합니다 (필수)</div>
+          <div
+            id="join_membership"
+            onClick={() => handleDivClick("/login/success")}
+          >
+            <button id="joinBtn" type="submit">
+              안심나잇 회원가입하기
+            </button>
           </div>
-          <div id="check1">
-            <img
-              id="terms_check"
-              src={`${process.env.PUBLIC_URL}/photos/gray.svg`}
-            />
-            <div id="terms_text">개인정보 수집 및 이용에 동의합니다 (필수)</div>
-          </div>
-          <div id="check1">
-            <img
-              id="terms_check"
-              src={`${process.env.PUBLIC_URL}/photos/gray.svg`}
-            />
-            <div id="member_text">위치정보 수집에 동의합니다 (선택)</div>
-          </div>
-        </div>
-        <div
-          id="join_membership"
-          onClick={() => handleDivClick("/login/success")}
-        >
-          <div id="joinBtn">안심나잇 회원가입하기</div>
-        </div>
-      </J.checkBoxs>
-      <J.Bottom>
-        <div id="bottom_bar"></div>
-      </J.Bottom>
+        </J.checkBoxs>
+        <J.Bottom>
+          <div id="bottom_bar"></div>
+        </J.Bottom>
+      </form>
+      {message && <p>{message}</p>}
     </J.Container>
   );
 };
